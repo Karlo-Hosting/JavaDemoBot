21cecd54aa0595942ad5878de2d10f35c2a1c68f
TheMiCraft <TheMiCraft@users.noreply.github.com>
2022-07-19 13:14:36 +0200

    init

---
+++ src/main/java/com/karlohosting/demo/DemoCommand.java
@@ -0,0 +1,12 @@
+package com.karlohosting.demo;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class DemoCommand extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (!event.getName().equals("ping")) return;
+        event.reply("Pong!").queue();
+    }
+}
---
+++ src/main/java/com/karlohosting/demo/JavaDemoBot.java
@@ -0,0 +1,26 @@
+package com.karlohosting.demo;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class JavaDemoBot {
+    JDA jda;
+    public static void main(String[] args) throws LoginException {
+        new JavaDemoBot();
+    }
+    public JavaDemoBot() throws LoginException {
+        JDABuilder builder = JDABuilder.createLight("OTk4ODk2OTk3MjkxNTMyMzE4.GiWrUM.VSuo1UBIV_dJ5xTz73GyVAYOIETCHbC3D-CAtw");
+        builder.setActivity(Activity.playing("on Karlo Hosting."));
+        builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
+        builder.addEventListeners(new DemoCommand());
+        builder.setEnabledIntents(GatewayIntent.MESSAGE_CONTENT);
+        jda = builder.build();
+        jda.upsertCommand("ping", "Calculate ping of the bot.").queue();
+    }
+
+}
