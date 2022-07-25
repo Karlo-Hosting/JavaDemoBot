f21a1b84c2c87f1eaf775d3c6d2d7bd75214518a
RaiderRoss <83859448+RaiderRoss@users.noreply.github.com>
2022-07-25 11:53:41 +0200

    Update DemoCommand.java

--- src/main/java/com/karlohosting/demo/DemoCommand.java
+++ src/main/java/com/karlohosting/demo/DemoCommand.java
@@ -1,12 +1,21 @@
-package com.karlohosting.demo;
-
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 public class DemoCommand extends ListenerAdapter {
+    //Text Command
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        if(event.getName().equals("ping")) {
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if(event.getMessage().equals("ping")){
+            event.getChannel().sendMessage("pong!").queue();
+        }
+    }
+    
+    //Slash Command
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if(event.getName().equals("png")){
             event.reply("pong!").queue();
         }
     }
